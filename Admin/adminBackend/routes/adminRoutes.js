34bd41d8d796d0f3762e47dff4bfa828e052a7a6
Home <ishipigera@gmail.com>
2022-07-27 04:00:47 +0530

    temparory user created

--- Admin/adminBackend/routes/adminRoutes.js
+++ Admin/adminBackend/routes/adminRoutes.js
@@ -2,12 +2,16 @@ const express = require('express')
 const router = express.Router()
 const AdminLogin = require('../controller/adminController')
 
- 
+const { postUser, getUser, getATempUser} = require('../controller/tempUserController')
  
 /*Login */
 router.post("/login", AdminLogin)
 
- 
+/*Create user routes */
+
+router.post("/user/create", postUser)
+router.get("/user/get", getUser)
+router.get("/user/get/:id", getATempUser)
 
 module.exports = router
 
