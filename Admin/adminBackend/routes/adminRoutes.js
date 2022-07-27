5d897ca8503b29e85f234d9bf9dfd4721564b057
Home <ishipigera@gmail.com>
2022-07-27 16:58:23 +0530

    Send email to the student after creating account

--- Admin/adminBackend/routes/adminRoutes.js
+++ Admin/adminBackend/routes/adminRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const AdminLogin = require('../controller/adminController')
 
-const { postUser, getUser, getATempUser} = require('../controller/tempUserController')
+const { postUser, getUser, getATempUser ,validateEmail} = require('../controller/tempUserController')
  
 /*Login */
 router.post("/login", AdminLogin)
@@ -12,6 +12,6 @@ router.post("/login", AdminLogin)
 router.post("/user/create", postUser)
 router.get("/user/get", getUser)
 router.get("/user/get/:id", getATempUser)
+router.post("/user/find", validateEmail)
 
 module.exports = router
-
